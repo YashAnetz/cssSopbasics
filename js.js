cbc734c23ea66b2613aa5c875d8ac34a4d5b5427
YashAnetz <55511290+YashAnetz@users.noreply.github.com>
2021-09-22 13:26:37 +0530

    Create js.js

---
+++ js.js
@@ -0,0 +1,8 @@
+let changeColourButton = document.querySelector(".changebutton")
+let texttochange = document.querySelector(".text")
+
+
+
+changeColourButton.addEventListener('click' , function(){
+    texttochange.style = 'color:tomato'
+})
